624d40b0e822a3a7e6e93f940bfa9d571abf0f52
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-06-27 18:15:40 -0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        blog1: resolve(__dirname, "blog", "blog-post-1.html"),
+        blog2: resolve(__dirname, "blog/blog-post-2.html"),
+        blog3: resolve(__dirname, "blog/blog-post-3.html"),
+      },
+    },
+  },
+});
